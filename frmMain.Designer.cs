43a7245160bafa02700469ce1a703c8e1c6dd0d3
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-06-08 21:44:58 +0500

    Setup the environment

---
+++ frmMain.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Vidyakali
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblScoreDisplay = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.lblLevelDisplay = new System.Windows.Forms.Label();
+            this.gameLoop = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(753, 23);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(34, 25);
+            this.lblScore.TabIndex = 3;
+            this.lblScore.Text = "10";
+            // 
+            // lblScoreDisplay
+            // 
+            this.lblScoreDisplay.AutoSize = true;
+            this.lblScoreDisplay.BackColor = System.Drawing.Color.Transparent;
+            this.lblScoreDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreDisplay.Location = new System.Drawing.Point(672, 23);
+            this.lblScoreDisplay.Name = "lblScoreDisplay";
+            this.lblScoreDisplay.Size = new System.Drawing.Size(75, 25);
+            this.lblScoreDisplay.TabIndex = 4;
+            this.lblScoreDisplay.Text = "Score :";
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.BackColor = System.Drawing.Color.Transparent;
+            this.lblLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLevel.Location = new System.Drawing.Point(78, 24);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(34, 25);
+            this.lblLevel.TabIndex = 3;
+            this.lblLevel.Text = "01";
+            // 
+            // lblLevelDisplay
+            // 
+            this.lblLevelDisplay.AutoSize = true;
+            this.lblLevelDisplay.BackColor = System.Drawing.Color.Transparent;
+            this.lblLevelDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLevelDisplay.Location = new System.Drawing.Point(12, 23);
+            this.lblLevelDisplay.Name = "lblLevelDisplay";
+            this.lblLevelDisplay.Size = new System.Drawing.Size(70, 25);
+            this.lblLevelDisplay.TabIndex = 4;
+            this.lblLevelDisplay.Text = "Level :";
+            // 
+            // gameLoop
+            // 
+            this.gameLoop.Enabled = true;
+            this.gameLoop.Interval = 30;
+            this.gameLoop.Tick += new System.EventHandler(this.gameLoop_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(0, 457);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(807, 35);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(807, 35);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(125, -58);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(538, 157);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(677, 20);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(12, 20);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(801, 494);
+            this.Controls.Add(this.lblLevelDisplay);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.lblScoreDisplay);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblScoreDisplay;
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.Label lblLevelDisplay;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer gameLoop;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EZInput;
+
+namespace Vidyakali
+{
+    public partial class frmMain : Form
+    {
+        //PictureBox playerIdle;
+        //PictureBox playerRunLeft;
+        //PictureBox playerRunRight;
+        PictureBox playerMovement;
+        private int PlayerMovementStatus=0;
+        PictureBox enemyIdel;
+        Random random;
+        private string enemyDirection="left";
+        private int enemySpeed=0;
+        List<PictureBox> playerFire = new List<PictureBox>();
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        
+        private void gameLoop_Tick(object sender, EventArgs e)
+        {
+
+            PlayerMovementStatus = 1;
+            if (Keyboard.IsKeyPressed(Key.LeftArrow))
+            {
+                PlayerMovementStatus = 1;
+                playerMovement.Left -= 10;
+            }
+            if (Keyboard.IsKeyPressed(Key.RightArrow))
+            {
+                PlayerMovementStatus = 2;
+                playerMovement.Left += 10;
+            }
+            if (Keyboard.IsKeyPressed(Key.UpArrow))
+            {
+                playerMovement.Top -= 10;
+            }
+            if (Keyboard.IsKeyPressed(Key.DownArrow))
+            {
+                playerMovement.Top += 10;
+            }
+
+            if (Keyboard.IsKeyPressed(Key.Space))
+            {
+                createBullet();
+            }
+            if (enemyIdel.Left <= 0)
+            {
+                enemyDirection = "right";
+            }
+            if (enemyIdel.Left + enemyIdel.Width >= this.Width)
+            {
+                enemyDirection = "left";
+            }
+
+            if (enemyDirection == "left")
+            {
+                enemyIdel.Left -= random.Next(0,enemySpeed);
+            }
+            if (enemyDirection == "right")
+            {
+                enemyIdel.Left += random.Next(0, enemySpeed);
+            }
+           
+
+        }
+
+       
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            createPlayer();
+            random = new Random();
+            enemySpeed = 5;
+            createEnemy();
+        }
+        private void createBullet()
+        {
+
+            PictureBox bullet = new PictureBox();
+            Image img = Vidyakali.Properties.Resources.bullet;
+            bullet.BackColor = Color.Transparent;
+            bullet.Image = img;
+            bullet.SizeMode = PictureBoxSizeMode.Zoom;
+            bullet.Left = playerMovement.Left+40;
+            bullet.Top =playerMovement.Top-10;
+            playerFire.Add(bullet);
+            this.Controls.Add(bullet);
+        }
+        private void createEnemy()
+        {
+            enemyIdel = new PictureBox();
+            Image img = Vidyakali.Properties.Resources.EnemyIdel;
+            enemyIdel.Image =img;
+            enemyIdel.SizeMode = PictureBoxSizeMode.Zoom;
+            enemyIdel.Top = random.Next(100,200);
+            enemyIdel.Left = random.Next(100,this.Width-img.Width);
+            this.Controls.Add(enemyIdel);
+            enemyDirection = "left";
+        }
+        private void createPlayer()
+        {
+            ////Create Idle player
+            //playerIdle = new PictureBox();
+            //Image img = Vidyakali.Properties.Resources.PlayerIdle;
+            //playerIdle.Image = img;
+            //playerIdle.SizeMode = PictureBoxSizeMode.Zoom;
+            //playerIdle.Left = this.Left;
+            //playerIdle.Top = this.Top;
+            //this.Controls.Add(playerIdle);
+            //Create run left player
+            //playerRunLeft = new PictureBox();
+            //playerRunLeft.Image = Vidyakali.Properties.Resources.PlayerRunLeft;
+            //playerRunLeft.SizeMode = PictureBoxSizeMode.Zoom;
+            //playerRunLeft.Left = this.Left;
+            //playerRunLeft.Top = this.Top;
+            //this.Controls.Add(playerRunLeft);
+            ////Create run Right player
+            //playerRunRight = new PictureBox();
+            //playerRunRight.Image = Vidyakali.Properties.Resources.PlayerRunRight;
+            //playerRunRight.SizeMode = PictureBoxSizeMode.Zoom;
+            //playerRunRight.Left = this.Left;
+            //playerRunRight.Top = this.Top;
+            //this.Controls.Add(playerRunRight);
+            //Create player Movement
+            playerMovement = new PictureBox();
+            if (PlayerMovementStatus == 0)
+            {//player idle
+                playerMovement.BackColor = Color.Transparent;
+                playerMovement.Image = Vidyakali.Properties.Resources.PlayerAttackLeft;
+            }
+            else if (PlayerMovementStatus == 1)
+            {//player  run left  
+                playerMovement.Image = Vidyakali.Properties.Resources.PlayerRunLeft;
+            }
+            else if (PlayerMovementStatus == 2)
+            {// player run right  
+                playerMovement.Image = Vidyakali.Properties.Resources.PlayerRunRight;
+            }
+            playerMovement.SizeMode = PictureBoxSizeMode.Zoom;
+            playerMovement.Left = this.Left;
+            playerMovement.Top = this.Top;
+            this.Controls.Add(playerMovement);
+        }
+    }
+}
+
